92c09303b7ad9f1bcdaef5ecd99ca76378d15d2e
hernan bordon <hernan.bordon@gmail.com>
2014-07-21 09:30:31 -0300

    Styling turns page

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,6 +1,8 @@
 class Turn < ActiveRecord::Base
 	belongs_to :user
 
-  scope :from_date, -> { where('date_turn >= ?', Time.now.strftime("%Y-%m-%d")) }
+  scope :from_date, -> { where('date_turn >= ?', Time.now.to_date-2) }
+
+  #scope :from_date, -> { where('date_turn >= ?', Time.now.strftime("%Y-%m-%d")) }
   scope :to_date, ->(f) { where('date_turn <= ?', f) }
 end
