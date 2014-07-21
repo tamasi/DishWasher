f4dd8c43d5f092ec08dfebbc648ddb29970ed822
hernan bordon <hernan.bordon@gmail.com>
2014-07-21 10:54:20 -0300

    changes in the scope for turns

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,8 +1,17 @@
 class Turn < ActiveRecord::Base
 	belongs_to :user
 
-  scope :from_date, -> { where('date_turn >= ?', Time.now.to_date-2) }
+  scope :from_date, -> { where('date_turn >= ?', date_turn_in_the_past(Time.now.to_date-2)).limit 7 }
 
   #scope :from_date, -> { where('date_turn >= ?', Time.now.strftime("%Y-%m-%d")) }
   scope :to_date, ->(f) { where('date_turn <= ?', f) }
+
+
+  def self.date_turn_in_the_past(date)
+    if date.strftime("%a") == "Sat" or "Sun"
+      date = date-2
+    end
+    return date
+  end
+
 end
