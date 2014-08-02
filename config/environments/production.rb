c73e60ceae6e3a5702c4b1620adeaaf59cf72acf
hernan bordon <hernan.bordon@gmail.com>
2014-08-02 13:41:01 -0300

    change raise_delivery_arrors to flase again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "dishwasher.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     addres: 'smtp.sendgrid.net',
     port: 587,
