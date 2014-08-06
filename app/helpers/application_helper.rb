73a861ca81730c9c16ba4d04e78bf47ba13b7d53
hernan bordon <hernan.bordon@gmail.com>
2014-08-06 12:22:46 -0300

    changes in home - refactoring code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,16 @@ module ApplicationHelper
 	def avatar_url(user)  
     	gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
     	"http://gravatar.com/avatar/#{gravatar_id}.png"  
-  	end  
+  end
+
+  def next_disher_style(index)
+    case index
+    when 3
+      'img-rounded tomorrow-washer falling-3'
+    when 4
+      'img-rounded today-washer falling-2'
+    else
+      "img-rounded next-washer falling-next-#{index}"
+    end
+  end
 end
