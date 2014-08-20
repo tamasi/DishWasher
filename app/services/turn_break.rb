679ad26d2b93864991c6f728eeb8b2d9a13148ee
hernan bordon <hernan.bordon@gmail.com>
2014-08-20 18:48:51 -0300

    changes in services change turn

--- app/services/turn_break.rb
+++ app/services/turn_break.rb
@@ -10,8 +10,26 @@ class TurnBreak
   def perform
     turns_break = Turn.from_users(user_ask_for_break.id).from_chose_date(start_date, end_date)
 
-    turns_break.each do |date_break|
-      turn_change = date_break.date_turn
+    turns_break.each_with_index do |turn_break, index|
+      next_index = index +1
+      turn_change = turn_break.date_turn
+      turn_break.destroy
+      while turns_break[next_index] != nil && turn_change <= turns_break[next_index].date_turn
+        next_turn_to_rotate = Turn.find_by(date_turn: turn_change)
+        next_turn_to_rotate.update(date_turn: turn_change)
+        next_workable_day(turn_change)
+      end
     end
+
+    puts 'fin'
   end
+
+  private
+    def next_workable_day(turn_change)
+      if turn_change.friday?
+        @turn_change += 3
+      else
+        @turn_change += 1
+      end
+    end
 end
