99ba06282720a461127e652e3a4df160460bf1a4
hernan bordon <hernan.bordon@gmail.com>
2014-08-25 15:45:45 -0300

    changes in break turn

--- app/services/turn_break.rb
+++ app/services/turn_break.rb
@@ -14,9 +14,14 @@ class TurnBreak
       next_index     = index + 1
       turn_to_change = turn.date_turn
       empty_day      = turn.date_turn
+      puts next_index
+      puts turn_to_change
+      puts turns_break[next_index].date_turn
       turn.destroy
       while empty_day <= turns_break[next_index].date_turn
-        next_workable_day(turn_to_change)
+        puts turn_to_change
+        next_workable_day
+        puts turn_to_change
         next_turn_to_rotate = Turn.for_organization(user_ask_for_break.organization).this_date(turn_to_change)
         next_turn_to_rotate.first.update(date_turn: empty_day)
         empty_day = turn_to_change
@@ -26,11 +31,12 @@ class TurnBreak
   end
 
   private
-    def next_workable_day(turn_to_change)
+    def next_workable_day
       if turn_to_change.friday?
         @turn_to_change += 3
       else
         @turn_to_change += 1
       end
+      puts @turn_to_change
     end
 end
