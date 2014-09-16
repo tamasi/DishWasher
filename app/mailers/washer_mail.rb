5d916cb7743448c3a69f010fa5de6779dcd3d180
hernan bordon <hernan.bordon@gmail.com>
2014-09-16 19:57:55 -0300

    Changes in mailer view

--- app/mailers/washer_mail.rb
+++ app/mailers/washer_mail.rb
@@ -1,8 +1,8 @@
 class WasherMail < ActionMailer::Base
 
 	def turn_mail(user, organization)
-		@userMail = user
-		@organization = organization
+    @userMail     = user
+    @organization = organization
 		mail(to: @userMail.email, subject: "Your turn to wash is. May the Force be with you")
 	end
 end
