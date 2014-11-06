7874ffa4a1c1a70a44796fe84415f43237986d62
hernan bordon <hernan.bordon@gmail.com>
2014-11-06 14:48:39 -0300

    Changes in rotate turn

--- app/services/rotate_turn_with_next_washer.rb
+++ app/services/rotate_turn_with_next_washer.rb
@@ -6,7 +6,7 @@ class RotateTurnWithNextWasher
   end
 
   def perform
-    turn_to_rotate = Turn.from_users(user.id).next_turn_to_rotate(Time.now.to_date).first
+    turn_to_rotate = Turn.from_users(@user.id).next_turn_to_rotate(Time.now.to_date).first
     next_turn      = Turn.next_turn_to_rotate(turn_to_rotate.date_turn)
     user_next_turn = next_turn[1].user
 
